409ea2b561073955c6511306a4d66d2f78c37738
James Arnold <j.arnold@buzzinteractive.co.uk>
2025-02-04 00:15:30 +0000

    bug: update missing tooltips and removed fields from property checkist

--- src/sidepanel/propertychecklist/propertyChecklist.ts
+++ src/sidepanel/propertychecklist/propertyChecklist.ts
@@ -46,26 +46,30 @@ export function generatePropertyChecklist(
     },
     {
       group: PropertyGroups.GENERAL,
-      label: "Listing history",
-      key: "listingHistory",
-      status: listingHistoryStatus,
-      value: listingHistoryValue,
-      askAgentMessage: "What's the listing history?",
+      label: "Location",
+      key: "location",
+      status: extractedData.location
+        ? DataStatus.FOUND_POSITIVE
+        : DataStatus.ASK_AGENT,
+      value: extractedData.location,
+      askAgentMessage: "Where's the property located?",
       toolTipExplainer:
-        "Listing history provides insights into the property's market activity, such as price changes and time on the market. This can indicate whether the property has been difficult to sell or if it has had price reductions.",
+        "Location is a critical factor in property valuation and desirability. It affects accessibility to amenities, schools, and transport links, and can influence the property's future value. A prime location can enhance lifestyle and convenience, while also impacting safety and community engagement.",
     },
     {
-      group: PropertyGroups.EXTERIOR,
-      label: "Parking",
-      key: "parking",
-      status: getYesNoOrMissingStatus(extractedData.parking),
-      value: extractedData.parking ?? "Ask agent",
-      askAgentMessage: "Is there parking?",
+      group: PropertyGroups.GENERAL,
+      label: "Property Type",
+      key: "propertyType",
+      status: extractedData.propertyType
+        ? DataStatus.FOUND_POSITIVE
+        : DataStatus.ASK_AGENT,
+      value: extractedData.propertyType,
+      askAgentMessage: "What's the property type?",
       toolTipExplainer:
-        "Parking can refer to how and where vehicles can be parked, and any associated costs. Factors to consider include whether a parking space is owned by you, if parking is communal, or if a permit is needed.",
+        "Property type refers to the category of the property, such as residential, commercial, or mixed-use. It can also include specific types like flats, houses, or apartments. Understanding the property type helps in assessing its value, potential use, and market demand.",
     },
     {
-      group: PropertyGroups.EXTERIOR,
+      group: PropertyGroups.GENERAL,
       label: "Accessibility",
       key: "accessibility",
       status:
@@ -78,6 +82,115 @@ export function generatePropertyChecklist(
         "Accessibility features make the property suitable for people with mobility needs. Common accessible features include level access, lift access, ramped access, wet rooms, wide doorways, step-free access, level access showers, and lateral living (a property where all key rooms are on the entry level).",
     },
     {
+      group: PropertyGroups.GENERAL,
+      label: "Listing history",
+      key: "listingHistory",
+      status: listingHistoryStatus,
+      value: listingHistoryValue,
+      askAgentMessage: "What's the listing history?",
+      toolTipExplainer:
+        "Listing history provides insights into the property's market activity, such as price changes and time on the market. This can indicate whether the property has been difficult to sell or if it has had price reductions.",
+    },
+    // Interior Details
+    {
+      group: PropertyGroups.INTERIOR,
+      label: "Bedrooms",
+      key: "bedrooms",
+      status: extractedData.bedrooms
+        ? DataStatus.FOUND_POSITIVE
+        : DataStatus.ASK_AGENT,
+      value: extractedData.bedrooms,
+      askAgentMessage: "How many bedrooms?",
+      toolTipExplainer:
+        "The number of bedrooms in a property is a key factor in its desirability and value. More bedrooms generally indicate a larger living space, which can be more valuable in the housing market.",
+    },
+    {
+      group: PropertyGroups.INTERIOR,
+      label: "Bathrooms",
+      key: "bathrooms",
+      status: extractedData.bathrooms
+        ? DataStatus.FOUND_POSITIVE
+        : DataStatus.ASK_AGENT,
+      value: extractedData.bathrooms,
+      askAgentMessage: "How many bathrooms?",
+      toolTipExplainer:
+        "The number of bathrooms in a property is a key factor in its desirability and value. More bathrooms generally indicate a larger living space, which can be more valuable in the housing market.",
+    },
+    {
+      group: PropertyGroups.INTERIOR,
+      label: "Heating Type",
+      key: "heatingType",
+      status:
+        typeof extractedData.heating === "string" &&
+        extractedData.heating.toLowerCase() !== agentMissingInfo
+          ? DataStatus.FOUND_POSITIVE
+          : DataStatus.ASK_AGENT,
+      value: extractedData.heating,
+      askAgentMessage: "What's the heating type?",
+      toolTipExplainer:
+        "Heating type refers to the method of heating used in the property, such as gas central heating, electric heating, or underfloor heating. It can also include specific types like boilers, radiators, or heat pumps. Understanding the heating type helps in assessing the property's energy efficiency and comfort.",
+    },
+    {
+      group: PropertyGroups.INTERIOR,
+      label: "Size",
+      key: "size",
+      status:
+        typeof extractedData.size === "string" &&
+        extractedData.size.toLowerCase() !== agentMissingInfo
+          ? DataStatus.FOUND_POSITIVE
+          : DataStatus.ASK_AGENT,
+      value: extractedData.size,
+      askAgentMessage: "What's the size?",
+      toolTipExplainer:
+        "The size of a property refers to the total area of the property, including all habitable rooms and spaces. It is a key factor in determining the property's value, as larger properties generally have higher value. Size can also impact the property's energy efficiency and maintenance costs.",
+    },
+    {
+      group: PropertyGroups.INTERIOR,
+      label: "Floor Plan",
+      key: "floorPlan",
+      status: extractedData.floorPlan
+        ? DataStatus.FOUND_POSITIVE
+        : DataStatus.ASK_AGENT,
+      value: DOMPurify.sanitize(extractedData.floorPlan ?? ""),
+      askAgentMessage: "Do you have a floor plan?",
+      toolTipExplainer:
+        "A floor plan is a detailed layout of the property's interior spaces, including rooms, hallways, and other features. It provides a visual representation of the property's layout and can be useful for understanding the property's size, layout, and potential for renovation or extension.",
+    },
+    {
+      group: PropertyGroups.EXTERIOR,
+      label: "Garden",
+      key: "garden",
+      status: getYesNoOrMissingStatus(extractedData.garden),
+      value: extractedData.garden ?? "Ask agent",
+      askAgentMessage: "Is there a garden?",
+      toolTipExplainer:
+        "A garden is a private outdoor space associated with a property, providing a place for relaxation, entertainment, and gardening. It can range from a small patio or balcony to a large garden with various features like lawns, trees, and outdoor living areas.",
+    },
+    {
+      group: PropertyGroups.EXTERIOR,
+      label: "Windows",
+      key: "windows",
+      status:
+        typeof extractedData.windows === "string" &&
+        extractedData.windows.toLowerCase() !== agentMissingInfo
+          ? DataStatus.FOUND_POSITIVE
+          : DataStatus.ASK_AGENT,
+      value: extractedData.windows,
+      askAgentMessage: "Windows - material & glazing?",
+      toolTipExplainer:
+        "The key information to know is the materials, such as wood, aluminium, or uPVC, and the glazing, such as single or double-glazed windows. Understanding the window material and glazing can impact the property's energy efficiency and comfort.",
+    },
+    {
+      group: PropertyGroups.EXTERIOR,
+      label: "Parking",
+      key: "parking",
+      status: getYesNoOrMissingStatus(extractedData.parking),
+      value: extractedData.parking ?? "Ask agent",
+      askAgentMessage: "Is there parking?",
+      toolTipExplainer:
+        "Parking can refer to how and where vehicles can be parked, and any associated costs. Factors to consider include whether a parking space is owned by you, if parking is communal, or if a permit is needed.",
+    },
+    {
       group: PropertyGroups.RIGHTS_AND_RESTRICTIONS,
       label: "Listed property",
       key: "listedProperty",
@@ -88,6 +201,16 @@ export function generatePropertyChecklist(
       askAgentMessage: "Is the property listed?",
       toolTipExplainer:
         "A listed property is designated as being of architectural or historical interest and requires special permission before being altered. There are three grades of listed buildings: Grade I (exceptional interest), Grade II* (more than special interest), and Grade II (special interest, most common for homes).",
+    },
+    {
+      group: PropertyGroups.RIGHTS_AND_RESTRICTIONS,
+      label: "Restrictions",
+      key: "restrictions",
+      status: getStatusFromBoolean(extractedData.restrictions, true),
+      value: getYesNoOrAskAgentStringFromBoolean(extractedData.restrictions),
+      askAgentMessage: "Any restrictions?",
+      toolTipExplainer:
+        "Restrictions are legal constraints on what can be done with the property, such as building height limits, conservation area regulations, or planning permissions. These restrictions can impact the property's value and potential use.",
     },
     {
       group: PropertyGroups.RIGHTS_AND_RESTRICTIONS,
@@ -131,6 +254,22 @@ export function generatePropertyChecklist(
     },
     {
       group: PropertyGroups.RISKS,
+      label: "Flood Sources",
+      key: "floodSources",
+      status:
+        (extractedData.floodSources ?? []).length > 0
+          ? DataStatus.FOUND_POSITIVE
+          : DataStatus.ASK_AGENT,
+      value:
+        (extractedData.floodSources ?? []).length > 0
+          ? (extractedData.floodSources?.join(", ") ?? "Ask agent")
+          : "Ask agent",
+      askAgentMessage: "Any flood sources?",
+      toolTipExplainer:
+        "Flood sources are the natural or man-made features that contribute to flooding, such as rivers, streams, dams, or levees. Understanding the flood sources can help assess the property's risk of flooding and the effectiveness of flood defences.",
+    },
+    {
+      group: PropertyGroups.RISKS,
       label: "Flooded in last 5 years",
       key: "floodedInLastFiveYears",
       status: getStatusFromBoolean(extractedData.floodedInLastFiveYears, true),
@@ -167,6 +306,18 @@ export function generatePropertyChecklist(
       askAgentMessage: "What council tax band?",
       toolTipExplainer:
         "Council tax is a payment to the local authority for services like schools and waste collection. Council tax bands are based on property value, and some exemptions apply (e.g., students).",
+    },
+    {
+      group: PropertyGroups.UTILITIES,
+      label: "Broadband",
+      key: "broadband",
+      status: extractedData.broadband
+        ? DataStatus.FOUND_POSITIVE
+        : DataStatus.ASK_AGENT,
+      value: extractedData.broadband,
+      askAgentMessage: "How's the broadband speed?",
+      toolTipExplainer:
+        "Broadband speed refers to the speed of internet connection, measured in megabits per second (Mbps). Faster speeds provide better internet connectivity, allowing for faster downloads, streaming, and online activities. It's important to check the broadband speed to ensure it meets your needs, especially for work, streaming, and gaming.",
     },
     // TODO: ON ROADMAP...
     // building safety
